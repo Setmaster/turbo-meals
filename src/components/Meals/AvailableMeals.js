eb6d94404819a60e3a8e55a18fd1bd0aa7568489
Victor <vi7org@hotmail.com>
2021-11-03 02:10:57 -0400

    Add Cart component, change dummy data in AvailableMeals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,27 +6,30 @@ import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
     {
         id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
+        name: 'Picanha Steak',
+        description: 'Finest steak seasoned with bold spices and seared\n' +
+            'just right with fries as side.',
         price: 22.99,
     },
     {
         id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
+        name: 'Bloomin\' Onion',
+        description: 'Our special crispy onion with dipping spicy sauce.',
         price: 16.5,
     },
     {
         id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
+        name: 'Springs Chicken',
+        description: 'Flame-grilled chicken breast topped with crisp\n' +
+            'strips of bacon, and our honey garlic sauce.',
+        price: 18.99,
     },
     {
         id: 'm4',
         name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
+        description: 'Chicken tenders served over mixed lettuce, with tomato,\n' +
+            'cheese, bacon, and homemade dressing.',
+        price: 10.99,
     },
 ];
 
