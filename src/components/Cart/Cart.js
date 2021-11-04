76644c92a5539f4dd6af9166add9f6188d28ec34
Victor <vi7org@hotmail.com>
2021-11-04 03:24:00 -0400

    Implement cart remove (-) button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,9 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
     const cartCtx = useContext(CartContext);
 
-    const cartItemRemoveHandler = id => {};
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+    };
 
     const cartItemAddHandler = item => {
         cartCtx.addItem({...item, amount: 1}); // copy item obj and add amount to it
